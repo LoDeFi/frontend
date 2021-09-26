026e7cd939f8c0074274de80e97b4295ef26c8ed
Nikita Schelkanov <nik.schelkanov@gmail.com>
2021-09-26 09:14:08 +0300

    trying to push

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react"
-import { client } from "defi-sdk"
-import styles from "./App.module.css"
-import { AddressInput } from "./components"
-import { useAddress } from "./hooks/useAddress"
-import { AssetsFlatList, AssetsProvider } from "./Assets"
-import { TxHistoryProvider, FlatTxList } from "./History"
-import { RepositoryProvider } from "./RepositoryContext"
-import { LodefiJson } from "./lodefi-types"
-import { LoyaltyProgramCurrentStatus, LoyaltyProgramsFlatList } from "./components/LoyaltyProgramShortInfo"
-import Favorite from "./components/Favorite"
+import React, { useState } from "react";
+import { client } from "defi-sdk";
+import styles from "./App.module.css";
+import { AddressInput } from "./components";
+import { useAddress } from "./hooks/useAddress";
+import { AssetsFlatList, AssetsProvider } from "./Assets";
+import { TxHistoryProvider, FlatTxList } from "./History";
+import { RepositoryProvider } from "./RepositoryContext";
+import { LodefiJson } from "./lodefi-types";
+import { LoyaltyProgramCurrentStatus, LoyaltyProgramsFlatList } from "./components/LoyaltyProgramShortInfo";
+import Favorite from "./components/Favorite";
 
-export const endpoint = "wss://api-staging.zerion.io"
-export const API_TOKEN = "Zerion.0JOY6zZTTw6yl5Cvz9sdmXc7d5AhzVMG"
+import Web3 from 'web3';
+import { useEffect } from "react";
+
+export const endpoint = "wss://api-staging.zerion.io";
+export const API_TOKEN = "Zerion.0JOY6zZTTw6yl5Cvz9sdmXc7d5AhzVMG";
+
+
+const loadBlockchainData = async () => {
+	const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+	const accounts = await web3.eth.getAccounts();
+	console.log("network", accounts[0])
+}
+
 
 client.configure({
 	url: endpoint,
@@ -28,6 +39,14 @@ Object.assign(window, { client })
 
 export const OldApp: React.FC = () =>
 {
+
+	useEffect(() => {
+		const accounts = loadBlockchainData();
+		setAddress("0x7e5ce10826ee167de897d262fcc9976f609ecd2b");
+		// вместо замоканного адреса из доки зериона 
+		// можно взять web3.eth.getAccounts()[0]
+	}, []);
+
 	const [address, setAddress] = useState<string | undefined>("")
 
 	const [selectedProgram, setSelectedProgram] = useState<LodefiJson>()
